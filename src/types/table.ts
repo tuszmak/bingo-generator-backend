4c33772729cb9129f946e597c47898c6ce0b55fd
Tuszmak <daniel.pinter02@gmail.com>
2025-02-18 14:57:17 +0100

    feat: add type validation

--- src/types/table.ts
+++ src/types/table.ts
@@ -1,11 +1,13 @@
+import { z } from "zod";
+
 export interface Table {
   content: string;
-  code: string;
   name: string;
 }
 
 export interface TableDAO extends Table {
   id: number;
+  code: string;
 }
 
 //Potential extension of the table. Export when applicable.
@@ -14,3 +16,8 @@ interface TableRatings {
   likes: number;
   submittedBy: string; //Actually this is a Username
 }
+
+export const TableReqSchema = z.object({
+  content: z.string(),
+  name: z.string(),
+});
