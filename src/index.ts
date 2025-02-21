5c3b3c5603c770d85d62cce3bbbff58678cfcc79
Tuszmak <daniel.pinter02@gmail.com>
2025-02-21 17:59:52 +0100

    feat: move routes to seperate file

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,10 @@
 import { serve } from "@hono/node-server";
-import { zValidator } from "@hono/zod-validator";
 import dotenvFlow from "dotenv-flow";
 import { Hono } from "hono";
-import type { ZodType } from "zod";
-import { createTable, findTableById } from "./repository/TableRepository.js";
-import { TableReqSchema, type Table } from "./types/table.js";
+import tables from "./routes/tables.js";
 
 dotenvFlow.config();
-const app = new Hono();
-
-app.get("/api/table/:id", async (c) => {
-  const id = c.req.param("id");
-  if (id) {
-    const table = await findTableById(id);
-    if (table) {
-      return c.body(JSON.stringify(table));
-    } else {
-      return c.notFound();
-    }
-  }
-});
-
-const defaultJsonValidatorFactory = <T extends ZodType>(schema: T) =>
-  zValidator("json", schema, (result, c) => {
-    if (!result.success) {
-      c.text("Invalid data!", 400);
-    }
-  });
-
-app.post(
-  "api/table",
-  defaultJsonValidatorFactory(TableReqSchema),
-  async (c) => {
-    const req = c.req.valid("json");
-    const { content, name }: Table = req;
-    const newTable = await createTable(content, name);
-    c.status(201);
-    return c.text(`Finished with id ${newTable.id}`);
-  }
-);
+const app = new Hono().route("/api/v1/table", tables);
 
 const port = 3000;
 console.log(`Server is running on http://localhost:${port}`);
---
+++ src/routes/tables.ts
@@ -0,0 +1,47 @@
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import type { ZodType } from "zod";
+import {
+  createTable,
+  findTableById,
+  getAllTables,
+} from "./../repository/TableRepository.js";
+import { TableReqSchema, type Table } from "./../types/table.js";
+
+const tables = new Hono();
+
+tables.get("/", async (c) => {
+  const tables = await getAllTables();
+  if (tables) {
+    return c.body(JSON.stringify(tables));
+  }
+});
+
+tables.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  if (id) {
+    const table = await findTableById(id);
+    if (table) {
+      return c.body(JSON.stringify(table));
+    } else {
+      return c.notFound();
+    }
+  }
+});
+
+const defaultJsonValidatorFactory = <T extends ZodType>(schema: T) =>
+  zValidator("json", schema, (result, c) => {
+    if (!result.success) {
+      c.text("Invalid data!", 400);
+    }
+  });
+
+tables.post("/", defaultJsonValidatorFactory(TableReqSchema), async (c) => {
+  const req = c.req.valid("json");
+  const { content, name }: Table = req;
+  const newTable = await createTable(content, name);
+  c.status(201);
+  return c.text(`Finished with id ${newTable.id}`);
+});
+
+export default tables;
