166581c2bb3230e867ac5817c4401fb419bbbacf
Tuszmak <daniel.pinter02@gmail.com>
2025-02-28 19:58:37 +0100

    refactor: remove not needed interface

--- src/types/table.ts
+++ src/types/table.ts
@@ -6,13 +6,6 @@ export interface Table {
   userName?: string;
 }
 
-//Potential extension of the table. Export when applicable.
-
-interface TableRatings {
-  likes: number;
-  submittedBy: string; //Actually this is a Username
-}
-
 export const TableReqSchema = z.object({
   content: z.string(),
   name: z.string(),
